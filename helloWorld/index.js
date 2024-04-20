c86d9fcf053630d61933dc2eebf53c34a1ac6529
koyamal <tanakamaruko1121n@gmail.com>
2024-04-20 18:40:05 +0900

    Firestoreを追加

--- helloWorld/index.js
+++ helloWorld/index.js
@@ -1,11 +1,18 @@
-import functions from '@google-cloud/functions-framework'
+import functions from '@google-cloud/functions-framework';
+import { Firestore } from '@google-cloud/firestore';
 import dotenv from 'dotenv';
 
 dotenv.config();
 const projectId = process.env.GCP_PROJECT;
+const gcpOptions = {
+  projectId,
+};
+
+const firestore = new Firestore(gcpOptions);
 
 // Register an HTTP function with the Functions Framework that will be executed
 // when you make an HTTP request to the deployed function's endpoint.
-functions.http('entryPoint', (req, res) => {
+functions.http('entryPoint', async (req, res) => {
+  const ref = await firestore.collection("test").get();
   res.send('Hello World!');
 });
