d2cc3a0a083569465f3f70c8e205fcd70a168deb
koyamal <tanakamaruko1121n@gmail.com>
2024-04-20 18:27:14 +0900

    modules化

--- helloWorld/index.js
+++ helloWorld/index.js
@@ -1,4 +1,8 @@
-const functions = require('@google-cloud/functions-framework');
+import functions from '@google-cloud/functions-framework'
+import dotenv from 'dotenv';
+
+dotenv.config();
+const projectId = process.env.GCP_PROJECT;
 
 // Register an HTTP function with the Functions Framework that will be executed
 // when you make an HTTP request to the deployed function's endpoint.
